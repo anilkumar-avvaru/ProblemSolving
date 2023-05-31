fdf86f4ad8b00166806610b0e2ef360093a28d48
avvaru.k.skuad <avvaru.k.skuad@aux.gojek.com>
2023-05-31 19:11:39 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import com.problems.DebugUtils;
+public class Main {
+    public static void main(String[] args) {
+        try{
+            int[][] myArray = {
+                    {0, 1, 2, 3, 123, 123, 123, 123},
+                    {12345, 123, 1234567, 123, 1, 123, 12, 12345678},
+                    {12, 12, 123, 12, 12, 123, 1, 1234}
+            };
+            DebugUtils du = new DebugUtils();
+            du.printTwoDimensionalArray(myArray, "myArray");
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/com/problems/DebugUtils.java
@@ -0,0 +1,61 @@
+package com.problems;
+public class DebugUtils {
+
+    private String getNCharString(int n, char ch){
+        String spacedString;
+        try{
+            spacedString = new String(new char[n]).replace('\0', ch);
+        }catch (Exception ex){
+            spacedString = "";
+        }
+        return spacedString;
+    }
+
+    public void printTwoDimensionalArray(int[][] arr) throws Exception{
+        prettyPrintTwoDimensionalArray(arr);
+    }
+
+    public void printTwoDimensionalArray(int[][] arr, String arrayName) throws Exception{
+        prettyPrintTwoDimensionalArray(arr, arrayName);
+    }
+
+    private int prettyPrintTwoDimensionalArray(int[][] arr) throws Exception{
+        return prettyPrintTwoDimensionalArray(arr, null);
+    }
+    private int prettyPrintTwoDimensionalArray(int[][] arr, String arrName) throws Exception{
+        try{
+            arrName = (arrName==null || arrName.trim().length()==0)?"--NoName--":arrName.trim();
+            int x = arr.length;
+            int y = arr[0].length;
+            int[] maxVerticalLengths = new int[y];
+            System.out.println("myArray = "+java.util.Arrays.toString(arr[0]));
+            for(int i=0; i<x; i++){
+                for(int j=0; j<y; j++){
+                    String tmp = Integer.toString(arr[i][j]);
+                    maxVerticalLengths[j] = Math.max(maxVerticalLengths[j], tmp.length());
+                }
+            }
+            System.out.println("maxVerticalLengths = "+java.util.Arrays.toString(maxVerticalLengths));
+            int dashes = 0;
+            for(int i=0; i<y; i++){
+                dashes += maxVerticalLengths[i];
+            }
+            String lineSeparator = getNCharString(dashes+y-1, '-');
+            System.out.println(lineSeparator);
+            System.out.println("The contents for the Array "+arrName+" are : ");
+            for(int i=0; i<x; i++){
+                for(int j=0; j<y; j++){
+                    String left = Integer.toString(arr[i][j]);
+                    String right = getNCharString(maxVerticalLengths[j]-left.length(), ' ');
+                    String result = left+right;
+                    System.out.print(result+" ");
+                }
+                System.out.println();
+            }
+            System.out.println(lineSeparator+"\n");
+        }catch (Exception ex){
+            return -1;
+        }
+        return 1;
+    }
+}
